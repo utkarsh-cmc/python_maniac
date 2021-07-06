35205895606f7eed3b4ca5764a52d7cbe67f8abf
Utkarsh Gumphekar <54374420+utkarsh-cmc@users.noreply.github.com>
2021-07-06 22:15:59 +0530

    loops problems

---
+++ loops_pbm1.py
@@ -0,0 +1,8 @@
+num=int(input("Enter the number : "))
+i=1
+# for i in range(1,11):
+#     print(str(num) + "X" + str(i)+"="+str(num*i))
+    
+    #or we can use f also
+for i in range(1,11):    
+    print(f"{num} X {i} = {num*i}")
